141cfc7e20458111a55b8100eb1d78335ce0a5a8
Abhisek Rath <iamabhisekrath@gmail.com>
2023-06-14 10:42:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-var a=prompt("Enter UserName");
- var b=prompt("Enter password");
-  	if(a== "abhisek" && b=="covid")
-  	{
-  	    window.location.href="start.html";
-  	}
-  	else
-  	{
-  		alert("invalid details");
-  		window.location.href="index.html";
-  	}
+
